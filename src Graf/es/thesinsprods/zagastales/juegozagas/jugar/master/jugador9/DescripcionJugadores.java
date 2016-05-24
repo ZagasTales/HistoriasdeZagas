8dbb9ddf5bcb762d1bc9dd8f840c649e3420d43b
ZagasTales <marcdelvallem@gmail.com>
2016-05-24 22:22:14 +0200

    Add files via upload

---
+++ src Graf/es/thesinsprods/zagastales/juegozagas/jugar/master/jugador9/DescripcionJugadores.java
@@ -0,0 +1,129 @@
+package es.thesinsprods.zagastales.juegozagas.jugar.master.jugador9;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+
+import es.thesinsprods.resources.font.MorpheusFont;
+import es.thesinsprods.zagastales.juegozagas.jugar.master.JugarOnline;
+
+import javax.swing.ImageIcon;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.Toolkit;
+
+public class DescripcionJugadores {
+
+	private JFrame frmHistoriasDeZagas;
+	public JFrame getFrame() {
+		return frmHistoriasDeZagas;
+	}
+
+	public void setFrame(JFrame frame) {
+		this.frmHistoriasDeZagas = frame;
+	}
+
+	MorpheusFont mf = new MorpheusFont();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DescripcionJugadores window = new DescripcionJugadores();
+					window.frmHistoriasDeZagas.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public DescripcionJugadores() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmHistoriasDeZagas = new JFrame();
+		frmHistoriasDeZagas.setTitle("Historias de Zagas");
+		frmHistoriasDeZagas.setIconImage(Toolkit.getDefaultToolkit().getImage(DescripcionJugadores.class.getResource("/images/Historias de Zagas, logo.png")));
+		frmHistoriasDeZagas.setResizable(false);
+		frmHistoriasDeZagas.setBounds(100, 100, 450, 385);
+		frmHistoriasDeZagas.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frmHistoriasDeZagas.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Descripci\u00F3n");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(mf.MyFont(0, 36));
+		lblNewLabel.setBounds(10, 11, 424, 69);
+		frmHistoriasDeZagas.getContentPane().add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 91, 424, 207);
+		frmHistoriasDeZagas.getContentPane().add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		String descripcionF = JugarOnline.personaje9.getDescription();
+		
+		if(!descripcionF.equals("") || !descripcionF.equals("null")){
+			textArea.setText(descripcionF);}
+		textArea.setFont(mf.MyFont(0, 12));
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		scrollPane.setViewportView(textArea);
+		
+		final JButton button = new JButton("");
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				button.setIcon(new ImageIcon(AtributosJugadores.class.getResource("/images/boton atras2.png")));
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				button.setIcon(new ImageIcon(AtributosJugadores.class.getResource("/images/boton atras.png")));
+			}
+		});
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmHistoriasDeZagas.dispose();
+				
+			}
+		});
+		button.setIcon(new ImageIcon(DescripcionJugadores.class.getResource("/images/boton atras.png")));
+		button.setOpaque(false);
+		button.setForeground(Color.WHITE);
+		button.setFocusPainted(false);
+		button.setContentAreaFilled(false);
+		button.setBorderPainted(false);
+		button.setBorder(new BevelBorder(BevelBorder.RAISED, null, null,
+								null, null));
+		button.setBackground(new Color(139, 69, 19));
+		button.setBounds(10, 309, 105, 35);
+		frmHistoriasDeZagas.getContentPane().add(button);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(DescripcionJugadores.class.getResource("/images/background-jugar.jpg")));
+		label.setBounds(0, 0, 444, 356);
+		frmHistoriasDeZagas.getContentPane().add(label);
+	}
+}
