8bac5a49126a77e9933fb5e7c3c3f77a8cfd735a
ZagasTales <marcdelvallem@gmail.com>
2016-05-29 06:19:57 +0200

    1.0.9.0

--- src/es/thesinsprods/zagastales/characters/equipment/Weapons.java
+++ src/es/thesinsprods/zagastales/characters/equipment/Weapons.java
@@ -70,4 +70,37 @@ public class Weapons extends Inventory {
 
 	}
 
+	public String atributo(){
+			
+		if(this.tipo.equals("Látigo")){return "Destreza";}
+		else if(this.tipo.equals("Arma de Puño")){return "Fuerza";}
+		else if(this.tipo.equals("Daga")){return "Destreza";}
+		else if(this.tipo.equals("Estoque")){return "Destreza";}
+		else if(this.tipo.equals("Espada")){return "Fuerza";}
+		else if(this.tipo.equals("Espada Curva")){return "Destreza";}
+		else if(this.tipo.equals("Katana")){return "Destreza";}
+		else if(this.tipo.equals("Hacha")){return "Fuerza";}
+		else if(this.tipo.equals("Maza")){return "Fuerza";}
+		else if(this.tipo.equals("Espadón")){return "Fuerza";}
+		else if(this.tipo.equals("Hacha de Guerra")){return "Fuerza";}
+		else if(this.tipo.equals("Martillo de Guerra")){return "Fuerza";}
+		else if(this.tipo.equals("Ultra-Espadón")){return "Fuerza";}
+		else if(this.tipo.equals("Lanza")){return "Destreza";}
+		else if(this.tipo.equals("Alabarda")){return "Fuerza";}
+		else if(this.tipo.equals("Bardiche")){return "Fuerza";}
+		else if(this.tipo.equals("Guadaña")){return "Destreza";}
+		else if(this.tipo.equals("Bastón de Combate")){return "Destreza";}
+		else if(this.tipo.equals("Martilo de Lucerna")){return "Fuerza";}
+		else if(this.tipo.equals("Arma Arrojadiza")){return "Destreza";}
+		else if(this.tipo.equals("Arco")){return "Destreza";}
+		else if(this.tipo.equals("Ballesta")){return "Destreza";}
+		else if(this.tipo.equals("Arma de Fuego")){return "Destreza";}
+		else if(this.tipo.equals("Escudo Pequeño")){return "Fuerza";}
+		else if(this.tipo.equals("Escudo Mediano")){return "Fuerza";}
+		else if(this.tipo.equals("Escudo de Torre")){return "Fuerza";}
+		
+		
+		
+		
+		return description;}
 }
